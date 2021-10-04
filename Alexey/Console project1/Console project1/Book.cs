954c5c76e297f056be978b40ae5f88715a5aaea3
DESKTOP-RN45RFU\user <alexeylavvin@gmail.com>
2021-10-04 16:21:09 +0300

    alexey folder

---
+++ Alexey/Console project1/Console project1/Book.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Console_project1
+{
+    class Book
+    {
+        public string AuthorName;
+
+        public string BookTitle;
+
+        public int Year;
+
+        public void GetInfo()
+        {
+            Console.WriteLine($"Imya avtora: (AuthorName) Nazvaniye knigi: (BookTitle) God: (Year)");
+        }
+    }
+}
+
---
+++ Alexey/Console project1/Console project1/Store.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Console_project1
+{
+    class Store
+    {
+        public string internet;
+        public string physical;
+
+    }
+}

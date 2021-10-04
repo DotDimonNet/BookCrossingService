ac7776f2f715191e5a4ee08e6e05471d8d5f3fdc
DESKTOP-33K4H4E\USER_PC_0096 <rkalinovych@gmail.com>
2021-10-04 06:17:39 -0700

    add

---
+++ Rostyslav/BookCrossingService/BookCrossingService/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BookCrossingService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

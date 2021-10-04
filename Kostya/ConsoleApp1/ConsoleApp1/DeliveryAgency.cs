6523aae7afcbcbb73bb2e6932f45eb4839a40f6d
DESKTOP-1ODO6LF\admin <magnum15930@gmail.com>
2021-10-04 16:19:11 +0300

    no message

---
+++ Kostya/ConsoleApp1/ConsoleApp1/DeliveryAgency.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class DeliveryAgency
+    {
+        public string Name { get; set; }
+        //Store[] Stores;
+        //User[] Users;
+        public DeliveryAgency(string name/*, Store[] stores, User[] users*/)
+        {
+            Name = name;
+            //Stores = stores;
+           // Users = users;
+        }
+        void Load()
+        {
+            Console.WriteLine("Loading books");
+        }
+        void Transport()
+        {
+            Console.WriteLine("Transporting books");
+        }
+        void Unload()
+        {
+            Console.WriteLine("Unloading books");
+        }
+        public void Deliver(User user, Book[] books)
+        {
+            Load();
+            Transport();
+            Unload();
+            user.GetBook(books);
+        }
+    }
+}
---
+++ Kostya/ConsoleApp1/ConsoleApp1/Store.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Store
+    {
+        public string Name { get; set; }
+        IEnumerable<Book> Books { get; set; }
+        DeliveryAgency DeliveryAgency { get; set; }
+        public Store(string name, Book[] books, DeliveryAgency deliveryAgency)
+        {
+            Name = name;
+            Books = books;
+            DeliveryAgency = deliveryAgency;
+        }
+        void GetBook(Book book)
+        {
+            Console.WriteLine("Книга отримана");
+        }
+        public void SendBooks(string[] bookNames, User user)
+        {
+            Console.WriteLine("Books prepeared for sending by Agency '" + DeliveryAgency.Name + "'");
+            //DeliveryAgency.Deliver(user, bookNames);
+        }
+        public void sendBook(string bookName)
+        {
+            Books = Books.Where(x => x.Name != bookName);
+        }
+    }
+}

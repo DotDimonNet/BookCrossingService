43e783c6d991bf7abf63c36719931bbd5d6a359d
njqdj <sergejminaev18@gmail.com>
2021-10-04 16:17:43 +0300

    vlad

---
+++ Vlad/ConsoleApp1/ConsoleApp1/Store.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    abstract class Store:IStore
+    {
+        public void Delivery()
+        {
+            Console.WriteLine("Delivering");
+        }
+
+    }
+}

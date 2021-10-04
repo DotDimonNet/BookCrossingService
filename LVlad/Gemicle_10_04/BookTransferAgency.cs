bfd56f8b6b9bea17c17b5075a1839b6a5dfd5273
DESKTOP-ROGE0VO\USER_PC_0383 <2001vlad2908@gmail.com>
2021-10-04 06:02:43 -0700

    First commit by Vlad

---
+++ LVlad/Gemicle_10_04/BookTransferAgency.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gemicle_10_04
+{
+    class BookTransferAgency
+    {
+        public Store[] Stores { get; set; }
+        public User[] Users { get; set; }
+        public string AgencyName { get; set; }
+
+        public void PrintStoresList(object[] obj)
+        {
+            int i = 1;
+            foreach(var st in Stores)
+            {
+                Console.WriteLine(i + ": " + st);
+                i++;
+            }
+        }
+        
+
+    }
+}
